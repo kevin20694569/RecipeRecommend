acd0a963fea478d1e8b682bb0e4b9f4f1c112c0c
kevin20694569 <kevin20694569@gmail.com>
2024-04-28 23:34:47 +0800

    Initial Commit

---
+++ DishGenerate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DishGenerate
+//
+//  Created by 黃楷程 on 2024/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
